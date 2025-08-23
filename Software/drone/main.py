ed3d552ed595f099d1cb6fb67b7df4f0c23b243f
FairyStriker <818mike@naver.com>
2025-08-24 01:55:59 +0900

    Update main.py

--- Software/drone/main.py
+++ Software/drone/main.py
@@ -36,29 +36,24 @@ patrol = Patrol(drone_system: IntegratedDroneSystem, fire_detector, landing, com
 # --- 메인 로직: PC로부터 명령 수신 대기 ---
 def main():
     communicator.start_receiver()
-        while True:
-            data = communicator.receive_data()
-            if data:
-                try:
-                    command_data = json.loads(data.decode('utf-8'))
-                        
-                    if isinstance(command_data, dict) and command_data.get('type') == 'EXTINGUISH':
-                        target = command_data.get('target')
-                        mission_thread = threading.Thread(target=run_extinguish_mission, args=(target,), daemon=True)   #화제 진압 알고리즘 run_extinguish_mission에 넣기
-                        mission_thread.start()
-                    elif isinstance(command_data, list):
-                        mission_thread = threading.Thread(target=run_mission, args=(command_data,), daemon=True)    #순찰 알고리즘 run_mission에 넣기
-                        mission_thread.start()
-                    else:
-                        print(f"알 수 없는 형식의 명령 수신: {command_data}")
+    while True:
+        data = communicator.receive_data()
+        if data:
+            try:
+                command_data = json.loads(data.decode('utf-8'))
 
-                except Exception as e:
-                    print(f"명령 처리 중 오류 발생: {e}")
+                if isinstance(command_data, dict) and command_data.get('type') == 'EXTINGUISH':
+                    target = command_data.get('target')
+                    mission_thread = threading.Thread(target=run_extinguish_mission, args=(target,), daemon=True)   #화제 진압 알고리즘 run_extinguish_mission에 넣기
+                    mission_thread.start()
+                elif isinstance(command_data, list):
+                    mission_thread = threading.Thread(target=run_mission, args=(command_data,), daemon=True)    #순찰 알고리즘 run_mission에 넣기
+                    mission_thread.start()
+                else:
+                    print(f"알 수 없는 형식의 명령 수신: {command_data}")
+
+            except Exception as e:
+                print(f"명령 처리 중 오류 발생: {e}")
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
