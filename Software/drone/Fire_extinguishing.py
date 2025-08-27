9bbe1138a39ecf92cc414cee856e4fe6069d002f
FairyStriker <818mike@naver.com>
2025-08-27 12:41:29 +0900

    Update Fire_extinguishing.py

--- Software/drone/Fire_extinguishing.py
+++ Software/drone/Fire_extinguishing.py
@@ -6,18 +6,26 @@ import cv2
 from MG90 import MG90
 
 class Fire_extinguishing():
-    def __init__(self, drone_system, fire_detector, landing, communicator):
+    def __init__(self, drone_system, fire_detector, landing, communicator, servo):
         self.drone_system = drone_system
         self.fire_detector = fire_detector
         self.landing = landing
         self.communicator = communicator
         self.ball_count = 2
+        self.servo = servo
 
     def drop_ball(self):
         self.ball_count -=1
+        self.servo.set_angle(180)
+        time.sleep(3)
+        self.servo.set_angle(0)
+        time.sleep(3)
+        self.servo.set_angle(90)
+        time.sleep(1)
 
     def run(self,target):
         self.ball_count = 2
+        self.servo.set_angle(90)
         #여기에 gps(target) 지점으로 이동하는 함수 이동 완료시 전까지 다음 while 문으로 안넘어가야함
         while not align:
             align, cmd = align_drone_to_object()
@@ -26,8 +34,8 @@ class Fire_extinguishing():
                 break
         if align:
             self.drop_ball()
-            time.sleep(10)
-
+            
         #스테이션으로 복귀하는 함
         self.landing.run()
+        self.servo.set_angle(0)
         self.communicator.send_status_update("EXTINGUISH_COMPLETE", ball_count)
